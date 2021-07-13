37e57e74182857efdc2028a424394eba4550d195
pptech-ds <pptech.ds@gmail.com>
2021-07-13 13:43:48 +0200

    ressource management fixed

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Ressource;
+use App\Form\RessourceFormType;
 use App\Entity\RessourceCategory;
 use App\Form\RessourceCategoryFormType;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,24 +26,52 @@ class AdminController extends AbstractController
         ]);
     }
 
+
+
+
     /**
-			 * @Route("/ressource_category/add", name="ressource_category_add")
-			 */
-			public function addCategory(Request $request): Response
-			{
-				$ressourceCategory = new RessourceCategory();
-				$form = $this->createForm(RessourceCategoryFormType::class, $ressourceCategory);
+     * @Route("/ressource_category/add", name="ressource_category_add")
+     */
+    public function addRessourceCategory(Request $request): Response
+    {
+        $ressourceCategory = new RessourceCategory();
+        $form = $this->createForm(RessourceCategoryFormType::class, $ressourceCategory);
 
-				$form->handleRequest($request);
-				if ($form->isSubmitted() && $form->isValid()) {
-					$em = $this->getDoctrine()->getManager();
-					$em->persist($ressourceCategory);
-					$em->flush();
-					return $this->redirectToRoute('admin_ressource_category_add');
-				}
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ressourceCategory);
+            $em->flush();
+            return $this->redirectToRoute('admin_ressource_category_add');
+        }
 
-				return $this->render('admin/ressource_category/add.html.twig', [
-					'form' => $form->createView(),
-				]);
-			}
+        return $this->render('admin/ressource_category/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/ressource/add", name="ressource_add")
+     */
+    public function addRessource(Request $request): Response
+    {
+        $ressource = new Ressource();
+        $form = $this->createForm(RessourceFormType::class, $ressource);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ressource->setUser($this->getUser());
+            $ressource->setActive(false);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($ressource);
+            $em->flush();
+            return $this->redirectToRoute('admin_ressource_add');
+        }
+
+        return $this->render('admin/ressource/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
