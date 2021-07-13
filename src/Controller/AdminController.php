5298c6778f7878ad761f50fd32b3eea02a1b7f4b
pptech-ds <pptech.ds@gmail.com>
2021-07-13 11:57:47 +0200

    ressourceCategory management fixed

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\RessourceCategory;
+use App\Form\RessourceCategoryFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
 * @Route("/admin", name="admin_")
@@ -20,4 +23,25 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    /**
+			 * @Route("/ressource_category/add", name="ressource_category_add")
+			 */
+			public function addCategory(Request $request): Response
+			{
+				$ressourceCategory = new RessourceCategory();
+				$form = $this->createForm(RessourceCategoryFormType::class, $ressourceCategory);
+
+				$form->handleRequest($request);
+				if ($form->isSubmitted() && $form->isValid()) {
+					$em = $this->getDoctrine()->getManager();
+					$em->persist($ressourceCategory);
+					$em->flush();
+					return $this->redirectToRoute('admin_ressource_category_add');
+				}
+
+				return $this->render('admin/ressource_category/add.html.twig', [
+					'form' => $form->createView(),
+				]);
+			}
 }
