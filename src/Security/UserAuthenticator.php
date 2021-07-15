683be510c61428f2c73e59efb92a1a224377da80
pptech-ds <pptech.ds@gmail.com>
2021-07-15 11:29:04 +0200

    script to regenerate DB added in composer.json and Authenticator optimzed

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -2,19 +2,23 @@
 
 namespace App\Security;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 class UserAuthenticator extends AbstractLoginFormAuthenticator
 {
@@ -23,10 +27,16 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
     public const LOGIN_ROUTE = 'app_login';
 
     private UrlGeneratorInterface $urlGenerator;
+    private $userRepository;
+    private $flashBag;
+    private $tokenStorage;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    public function __construct(UrlGeneratorInterface $urlGenerator, FlashBagInterface $flashBag, UserRepository $userRepository, TokenStorageInterface $tokenStorage)
     {
         $this->urlGenerator = $urlGenerator;
+        $this->userRepository = $userRepository;
+        $this->flashBag = $flashBag;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function authenticate(Request $request): PassportInterface
@@ -50,7 +60,15 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
+        $user = $this->userRepository->findOneBy(['email' => $request->request->get('email')]);
+
+        if (!$user->isVerified()) {
+            $this->tokenStorage->setToken(new AnonymousToken('default', 'anon.'));
+            $request->getSession()->invalidate();
+            $this->flashBag->add('error', 'Vous devez valider votre adresse e-mail pour pouvoir vous connecter');
+
+            return new RedirectResponse($this->urlGenerator->generate('app_login'));
+        }
         return new RedirectResponse($this->urlGenerator->generate('home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
