6bb5100b18bd55b1cee805100bd5f837d9b1377f
pptech-ds <pptech.ds@gmail.com>
2021-07-26 12:14:31 +0200

    datafixtures updated

--- src/DataFixtures/WebinarFixtures.php
+++ src/DataFixtures/WebinarFixtures.php
@@ -20,7 +20,7 @@ class WebinarFixtures extends Fixture implements DependentFixtureInterface
         for($i = 1; $i <= 200; $i++ ) {
             // getting references created in WebinarCategoryFixtures
             $webinarCategory = $this->getReference('webinarCategory_' . $faker->numberBetween(0, 1));
-            $user = $this->getReference('user_pro_' . $faker->numberBetween(11, 15));
+            $user = $this->getReference('user_pro_' . $faker->numberBetween(1, 10));
             $webinar = new Webinar;
             $webinar->setWebinarCategory($webinarCategory);
             $webinar->setTitle($faker->catchPhrase());
